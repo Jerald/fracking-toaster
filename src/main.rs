61d24d950fae624c76bf8c10faf42763a7103512
Oscar <osmithsi@sfu.ca>
2019-07-17 10:16:55 -0700

    Implemented basic ping bot for testing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use std::env;
+
+use serenity::prelude::*;
+
+use serenity::model::channel::Message;
+use serenity::framework::standard::{
+    StandardFramework,
+    CommandResult,
+    macros::{
+        command,
+        group
+    }
+};
+
+group!({
+    name: "general",
+    options: {},
+    commands: [ping],
+});
+
+struct Handler;
+impl EventHandler for Handler {}
+
 fn main() {
     println!("Hello, world!");
+
+    let mut client = Client::new(&env::var("DISCORD_TOKEN").expect("Didn't find DISCORD_TOKEN env variable!"), Handler)
+        .expect("Error creating client!");
+
+    client.with_framework(StandardFramework::new()
+        .configure(|c| c.prefix(">"))
+        .group(&GENERAL_GROUP));
+
+    if let Err(why) = client.start()
+    {
+        println!("An error occured while starting the client: {:?}", why);
+    }
+}
+
+#[command]
+fn ping(ctx: &mut Context, msg: &Message) -> CommandResult
+{
+    msg.reply(ctx, "Pong!")?;
+    Ok(())
 }
